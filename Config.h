a18e9755a9b9a0e0d2ca4a4aca1d058cf0bec324
skeleton0 <34125010+skeleton0@users.noreply.github.com>
2018-07-21 17:40:01 +1000

    Updated port number in config file

--- Config.h
+++ Config.h
@@ -1,6 +1,6 @@
 #define APN "hologram"
 #define SERVER_ADDR "14.201.37.10"
-#define SERVER_PORT "22000"
+#define SERVER_PORT "46000"
 #define DEVICE_ID "0"
 #define MOVING_UPDATE_FREQUENCY 5000 //5 seconds
 #define SITTING_UPDATE_FREQUENCY 500000 //5 minutes
