c4f2bea143cc2bc58c0781e7f89bb408efa4aa3e
skeleton0 <34125010+skeleton0@users.noreply.github.com>
2019-11-01 23:55:28 +1100

    Update config

--- Config.h
+++ Config.h
@@ -1,6 +1,6 @@
 #define APN "hologram"
-#define SERVER_ADDR "14.201.37.10"
-#define SERVER_PORT "46000"
+#define SERVER_ADDR "0.0.0.0"
+#define SERVER_PORT "0"
 #define DEVICE_ID "0"
 #define MOVING_UPDATE_FREQUENCY 5000 //5 seconds
 #define SITTING_UPDATE_FREQUENCY 600000 //10 minutes
